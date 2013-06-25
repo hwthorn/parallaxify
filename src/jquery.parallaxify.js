5c18e6e28a18855d751ebfe6d47125570ecc772b
Felix Pflaum <hwthorn@outlook.com>
2013-06-25 10:38:09 -0400

    added mobile device detection to improve inputPriority handling

--- src/jquery.parallaxify.js
+++ src/jquery.parallaxify.js
@@ -173,6 +173,7 @@
 			this._defineElements();
 			this._defineGetters();
 			this._defineSetters();
+			this._detectMobile();
 			this._detectMotionType();
 			this._detectViewport();
 			this._handleWindowLoadAndResize();
@@ -198,9 +199,6 @@
 			this._getMoveHorizontal = function() {
 
 				if (this.useMouseMove && this.clientX !== null && this.clientX !== this.oldClientX ) {
-					if (this.useSensor && this.options.inputPriority === 'mouse') {
-						this.useSensor = this.useSensorMoz = this.useSensorWebkit = false;
-					}
 					return mouseMoveAdapter( this.options.motionAngleX * (1 - (2*this.clientX/this.viewportWidth)), this.options.motionAngleX);
 				}
 
@@ -238,9 +236,6 @@
 			this._getMoveVertical = function() {
 
 				if (this.options.useMouseMove && this.clientY !== null && this.clientY !== this.oldClientY ) {
-					if (this.useSensor && this.options.inputPriority === 'mouse') {
-						this.useSensor = this.useSensorMoz = this.useSensorWebkit = false;
-					}
 					return mouseMoveAdapter( this.options.motionAngleY * (1 - (2*this.clientY/this.viewportHeight)), this.options.motionAngleY);
 				}
 
@@ -323,20 +318,25 @@
 				this.viewportLandscape = (Math.abs(window.orientation) === 90);
 			}
 		},
+		_detectMobile: function() {
+			// see http://detectmobilebrowser.com/mobile
+			var browser = navigator.userAgent || navigator.vendor || window.opera;
+			this.isMobile = (/(bb\d+|meego).+mobile|android|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|playbook|plucker|pocket|psp|series(4|6)0|silk|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(browser)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(browser.substr(0,4)));
+		},
 		_detectMotionType: function() {
 			this.useSensor = false;
 			this.useSensorWebkit = false;
 			this.useSensorMoz = false;
 			this.useMouseMove = false;
 
-			if (this.options.useGyroscope) {
+			if (this.options.useGyroscope && (this.isMobile || this.options.inputPriority === 'gyroscope')) {
 				// Webkit uses deviceorientation (DeviceOrientationEvent)
 				this.useSensorWebkit = window.DeviceOrientationEvent !== undefined;
 				// Mozilla uses MozOrientation (OrientationEvent)
 				this.useSensorMoz = window.OrientationEvent !== undefined;
 				this.useSensor = this.useSensorWebkit || this.useSensorMoz;
 			}
-			if (this.options.useMouseMove) {
+			if (this.options.useMouseMove && !this.isMobile) {
 				this.useMouseMove = this.$viewportElement.mousemove !== undefined;
 			}
 		},
@@ -484,6 +484,7 @@
 				this.initialBeta = beta;
 				if (this.useSensor && this.options.inputPriority === 'gyroscope') {
 					this.useMouseMove = false;
+					if (this.useMouseMove) this.$viewportElement.unbind('mousemove.' + this.name);
 				}
 			}
 
@@ -491,6 +492,7 @@
 				this.initialGamma = gamma;
 				if (this.useSensor && this.options.inputPriority === 'gyroscope') {
 					this.useMouseMove = false;
+					if (this.useMouseMove) this.$viewportElement.unbind('mousemove.' + this.name);
 				}
 			}
 
